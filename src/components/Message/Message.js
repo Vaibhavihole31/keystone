8a8c0a0af5675ca8ca168dd0b651d2435840ce03
Vaibhavihole31 <itspinki05@gmail.com>
2022-12-22 12:18:33 +0530

    feat: student list view added

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -9,14 +9,7 @@ function Message() {
            <div className='col-md-3'></div>
            <div className='col-md-6'>
              <div className='message-card'>
-             Lorem Ipsum is simply dummy text of 
-             the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard
-               dummy text ever since the 1500s, when an unknown
-                printer took a galley of type and scrambled 
-                 to make a type specimen book. It has survived
-                  not only five centuries, but also the leap into 
-                  electronic typesetting, remaining essentially unchanged. <br/><br/><br/>
+             <textarea className="form-control mb-2 p-2" placeholder='Enter Your Message' rows="5" name="message" required /> <br/><br/>
 
              <div className='row'>
               <div className='col-md-6'>
