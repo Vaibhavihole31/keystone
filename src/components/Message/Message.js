7936f9f3fe6892ac943cd4056fd85a8ffe3b825e
Vaibhavihole31 <itspinki05@gmail.com>
2023-02-07 23:29:43 +0530

    ref: msg card btn change

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -7,7 +7,7 @@ function Message() {
     <div className='container'>
         <div className='row'>
            <div className='col-md-3'></div>
-           <div className='col-md-8'>
+           <div className='col-md-6'>
              <div className='message-card'>
              <h3 className='text-center mb-5'>Quick Message</h3>
              <textarea className="form-control mb-2 p-2" placeholder='Enter Your Message' rows="5" name="message" required /> <br/><br/>
