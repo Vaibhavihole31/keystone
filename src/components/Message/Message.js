f46915dc4f3c31593db701778e466c294838f33e
Vaibhavihole31 <itspinki05@gmail.com>
2023-02-07 23:29:02 +0530

    ref: message card

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -7,12 +7,17 @@ function Message() {
     <div className='container'>
         <div className='row'>
            <div className='col-md-3'></div>
-           <div className='col-md-6'>
+           <div className='col-md-8'>
              <div className='message-card'>
              <h3 className='text-center mb-5'>Quick Message</h3>
              <textarea className="form-control mb-2 p-2" placeholder='Enter Your Message' rows="5" name="message" required /> <br/><br/>
 
              <div className='row'>
+             <div className='col-md-6'>
+              <div className='text-center'>
+                <button className='cancel-btn'><b>Cancel</b></button>
+                </div>
+             </div>
               <div className='col-md-6'>
                 <div>
                 <div className='text-center'>
@@ -20,11 +25,6 @@ function Message() {
                 </div>
                 </div>
               </div>
-              <div className='col-md-6'>
-              <div className='text-center'>
-                <button className='cancel-btn'><b>Cancel</b></button>
-                </div>
-             </div>
              </div>
              </div>
            </div>
